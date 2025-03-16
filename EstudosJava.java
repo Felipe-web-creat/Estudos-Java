aad862ede9cb4f08267107389c86008b2ce6d06d
Felipe Lopes <felipelopesofc00@gmail.com>
2025-03-16 18:11:12 +0000

    versão 1

---
+++ EstudosJava.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EstudosJava{
+    /*metodo pirnipal*/
+    public static void main(String[] args) {
+        
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Bem vindo ao programa de estudos de java");
+        System.out.println("Digite seu nome: ");
+        String nome = scanner1.nextLine();
+        System.out.println("Olá," + nome + "! Vamos começar");
+        
+        
+
+    }
+
+
+}
